cb2c3da8b12592915521b6070bae12a82fc0660b
Luna <lunaperera152@gmail.com>
2026-04-22 13:38:24 +0100

    holauu

---
+++ src/main/java/com/mycompany/refactoor/Refactoor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.refactoor;
+
+/**
+ *
+ * @author lunar
+ */
+public class Refactoor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
